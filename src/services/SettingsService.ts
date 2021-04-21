e7f0818daffd4cf33a8547e8411164e13fb40b65
Edson Shideki Kokado <eskokado@gmail.com>
2021-04-21 09:28:41 -0300

    separando a responsabilidade para o SettingsService

---
+++ src/services/SettingsService.ts
@@ -0,0 +1,28 @@
+﻿import { getCustomRepository } from "typeorm";
+import { SettingsRepository } from "../repositories/SettingsRepository";
+
+interface ISettingsCreate {
+  username: string;
+  chat: boolean;
+}
+
+class SettingsService {
+  async create({ chat, username }: ISettingsCreate) {
+    const settingsRepository = getCustomRepository(SettingsRepository);
+
+    const userAlreadyExists = await settingsRepository.findOne({ username });
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists!");
+    }
+
+    const setting = settingsRepository.create({
+      chat,
+      username,
+    });
+    await settingsRepository.save(setting);
+    return setting;
+  }
+}
+
+export { SettingsService };
