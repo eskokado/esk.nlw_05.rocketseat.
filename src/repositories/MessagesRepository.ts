36aede5920690eb9f34f2c95bb9f538b348cb177
Edson Shideki Kokado <eskokado@gmail.com>
2021-04-21 11:35:15 -0300

    Criando repositorio MessagesRepository

---
+++ src/repositories/MessagesRepository.ts
@@ -0,0 +1,7 @@
+﻿import { EntityRepository, Repository } from "typeorm";
+import { Message } from "../entities/Message";
+
+@EntityRepository(Message)
+class MessagesRepository extends Repository<Message> {}
+
+export { MessagesRepository };
