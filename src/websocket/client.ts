abf10a43f8280b880f1e7d523ea6dfea25cb09ee
Edson Shideki Kokado <eskokado@gmail.com>
2021-04-23 07:54:54 -0300

    Melhorando o código em client

--- src/websocket/client.ts
+++ src/websocket/client.ts
@@ -1,7 +1,40 @@
 ﻿import { io } from "../http";
+import { ConnectionsService } from "../services/ConnectionsService";
+import { UsersService } from "../services/UsersService";
+import { MessagesService } from "../services/MessagesService";
+import { User } from "../entities/User";
+
+interface IParams {
+  text: string;
+  email: string;
+}
 
 io.on("connect", (socket) => {
-  socket.on("client_first_access", (params) => {
-    console.log(params);
+  const connectionsService = new ConnectionsService();
+  const usersService = new UsersService();
+  const messagesService = new MessagesService();
+  socket.on("client_first_access", async (params: IParams) => {
+    const socket_id = socket.id;
+    const { text, email } = params;
+    let user: User = null;
+
+    const userExists = await usersService.findByEmail(email);
+
+    if (!userExists) {
+      user = await usersService.create(email);
+    } else {
+      user = userExists;
+    }
+    const connection = await connectionsService.findByUser(user);
+    if (!connection) {
+      await connectionsService.create({
+        socket_id,
+        user,
+      });
+    } else {
+      connection.socket_id = socket_id;
+      await connectionsService.create(connection);
+    }
+    await messagesService.create({ text, user_id: user.id });
   });
 });
